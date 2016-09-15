bfb77cd1a8ab267e2956704d1711b1779aaa8e21
Pedro Silva <pedro.silva@booking.com>
2016-09-15 10:41:44 +0200

    parse datatypes using a proper parser combinator

--- hbase-snapshotter/src/main/scala/com/booking/spark/DataTypeParser.scala
+++ hbase-snapshotter/src/main/scala/com/booking/spark/DataTypeParser.scala
@@ -1,27 +1,50 @@
 package com.booking.spark
 
+import scala.language.postfixOps
+import scala.util.matching._
+import scala.util.parsing.combinator._
 import org.apache.spark.sql.types._
 
-object DataTypeParser {
+case class MySQLPrecision(precision: Int, scale: Option[Int])
+case class MySQLDataType(typename: String, precision: Option[MySQLPrecision], qualifiers: Seq[String])
 
-  implicit class Regex(sc: StringContext) {
-    def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+object DataTypeParser extends JavaTokenParsers {
+
+  val typenames = Seq("MEDIUMBLOB", "MEDIUMTEXT", "MEDIUMINT", "TIMESTAMP", "VARBINARY", "DATETIME", "LONGBLOB", "LONGTEXT", "SMALLINT", "TINYBLOB", "TINYTEXT", "DECIMAL", "INTEGER", "NUMERIC", "TINYINT", "VARCHAR", "BIGINT", "BINARY", "DOUBLE", "FLOAT", "TIME", "BLOB", "CHAR", "DATE", "ENUM", "JSON", "REAL", "TEXT", "YEAR", "BIT", "INT", "SET")
+  val qualifiers = Seq("UNSIGNED", "ZEROFILL", "BINARY", "CHARACTER SET", "COLLATE")
+
+  def datatypeSpec: Parser[MySQLDataType] = typename ~ (precision?) ~ (qualifier*) ^^ {
+    case t ~ p ~ Seq(q) => new MySQLDataType(t, p, Seq(q))
+    case t ~ p ~ q => new MySQLDataType(t, p, q)
   }
 
-  def parse(spec: String): DataType = {
-    spec.toLowerCase().trim().split("""\W""")(0)  match {
-      case "tinyint"
-         | "smallint"
-         | "mediumint"
-         | "int"
-         | "integer"
-         | "timestamp" => IntegerType
-      case "bigint" => LongType
-      case "numeric"
-         | "decimal"
-         | "float"
-         | "double"
-         | "real" => DoubleType
+  def typename: Parser[String] = makeRegex(typenames, true)
+
+  def precision: Parser[MySQLPrecision] = "(" ~> wholeNumber ~ ("," ~> wholeNumber?) <~ ")" ^^ {
+    case l ~ Some(d) => new MySQLPrecision(l.toInt, Some(d.toInt))
+    case l ~ None => new MySQLPrecision(l.toInt, None)
+  }
+
+  def qualifier: Parser[String] = makeRegex(qualifiers, true)
+
+  def makeRegex(tokens: Seq[String], caseInsensitive: Boolean): Regex = {
+    val start = if (caseInsensitive) "(?i)" else ""
+    (start + tokens.mkString("(", "|", ")")).r
+  }
+
+  def apply(exprString: String): DataType = {
+     parseAll(datatypeSpec, exprString) match {
+       case Success(dt, _) => mySQLToSparkSQL(dt)
+       case Failure(msg, _) => NullType
+       case Error(msg, _) => NullType
+     }
+  }
+
+  def mySQLToSparkSQL(dt: MySQLDataType): DataType = {
+    dt.typename match {
+      case "TINYINT" | "SMALLINT" | "MEDIUMINT" | "INT" | "INTEGER" => IntegerType
+      case "BIGINT" => LongType
+      case "NUMERIC" | "DECIMAL" | "FLOAT" | "DOUBLE" | "REAL" => DoubleType
       case _ => StringType
     }
   }
