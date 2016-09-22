03ebfda2e8a0d55f9bbc62a849233f62ec8447bb
Pedro Silva <pedro.silva@booking.com>
2016-09-22 14:18:58 +0200

    remove spark-specific apply()

--- hbase-snapshotter/src/test/scala/com/booking/spark/DataTypeParserSpec.scala
+++ hbase-snapshotter/src/test/scala/com/booking/spark/DataTypeParserSpec.scala
@@ -1,14 +1,36 @@
-package com.booking.spark
+package com.booking.sql
 
 import org.scalatest._
 
-class DataTypeParserSpec extends FlatSpec with Matchers[] {
-  val queries: Seq[String] = Seq("INT",
-      "INT(128)",
-      "LONG UNSIGNED",
-      "INT UNSIGNED BINARY",
-      "INT (128) UNSIGNED",
-      "INT (128) UNSIGNED BINARY",
-      "INT (128, 1) UNSIGNED",
-      "INT (128, 6) UNSIGNED BINARY")
+class DataTypeParserSpec extends FlatSpec with Matchers {
+
+  "A DataTypeParser" should "return a MySQLDataType" in {
+    val dt: MySQLDataType = DataTypeParser("INT")
+    assert(dt.isInstanceOf[MySQLDataType])
+  }
+
+  it should "parse typename" in {
+    val dt: MySQLDataType = DataTypeParser("INT")
+    assert(dt.typename == "INT")
+  }
+
+  it should "parse precision" in {
+    val dt: MySQLDataType = DataTypeParser("DOUBLE(10)")
+    assert(dt.precision.get == MySQLPrecision(10, None))
+  }
+
+  it should "parse scale" in {
+    val dt: MySQLDataType = DataTypeParser("DOUBLE(10, 2)")
+    assert(dt.precision.get == MySQLPrecision(10, Some(2)))
+  }
+
+  it should "parse qualifiers" in {
+    val dt: MySQLDataType = DataTypeParser("INTEGER UNSIGNED ZEROFILL")
+    assert(dt.qualifiers == Seq("UNSIGNED", "ZEROFILL"))
+  }
+
+  it should "parse attributes" in {
+    val dt: MySQLDataType = DataTypeParser("INTEGER CHARACTER SET \"utf-8\" COLLATE \'latin1_bin\'")
+    assert(dt.attributes == Map(("COLLATE", "latin1_bin"), ("CHARACTER SET", "utf-8")))
+  }
 }
