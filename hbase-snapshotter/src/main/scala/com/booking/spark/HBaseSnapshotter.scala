780bea913186c09366f7a9ca5436ac0e5e51e531
Pedro Silva <pedro.silva@booking.com>
2016-09-30 14:53:33 +0200

    first rc

--- hbase-snapshotter/src/main/scala/com/booking/spark/HBaseSnapshotter.scala
+++ hbase-snapshotter/src/main/scala/com/booking/spark/HBaseSnapshotter.scala
@@ -82,60 +82,94 @@ object HBaseSnapshotter {
     }
   }
 
-  def transformSchema(table: String, value: String): Seq[StructField] = {
-    val o = new JsonParser().parse(value).getAsJsonObject().getAsJsonObject(table)
-    o.getAsJsonObject("columnIndexToNameMap").entrySet().toSeq.map({ x => {
-      val columnIndex: Int = x.getKey().toInt
-      val columnName: String = x.getValue().getAsString()
-      val columnType: String = o.getAsJsonObject("columnsSchema")
-        .getAsJsonObject(columnName)
-        .getAsJsonPrimitive("columnType")
-        .getAsString()
-      (columnIndex, columnName, columnType)
-    }}).sortBy(_._1).map({ x => StructField(x._2, mySQLToSparkSQL(x._3), true) })
+
+  /** Parse schema information from MySQL to HBase dump
+    * @param table original MySQL table name
+    * @param value json object with following structure:
+    *     {table => { "columnIndexToNameMap": { index: name, ... },
+    *                 "columnsSchema": { name: { "columnType": "sometype", ... }}}}
+    * @return Spark schema
+    */
+  def transformSchema(table: String, value: String): StructType = {
+    println(table)
+    println(value)
+    val schemaObject = new JsonParser().parse(value)
+    val tableSchema = schemaObject.getAsJsonObject().getAsJsonObject(table)
+    val columnIndexToNameMap = tableSchema.getAsJsonObject("columnIndexToNameMap")
+    val columnsSchema = tableSchema.getAsJsonObject("columnsSchema")
+
+    val sortedSchema: Seq[(Int, String, String)] =
+      columnIndexToNameMap.entrySet().toSeq.map({ idx => {
+        val columnIndex = idx.getKey().toInt
+        val columnName = idx.getValue().getAsString()
+        val columnType = columnsSchema
+          .getAsJsonObject(columnName)
+          .getAsJsonPrimitive("columnType")
+          .getAsString()
+        (columnIndex, columnName, columnType)
+      }}).sortBy(_._1)
+
+    return StructType(sortedSchema.map({ field =>
+      StructField(field._2, mySQLToSparkSQL(field._3), true)
+    }))
   }
 
-  def getSchema(tableName: String): StructType = {
-    val filters = new FilterList(
-      FilterList.Operator.MUST_PASS_ALL,
-      new FirstKeyOnlyFilter()// ,
-                              // new KeyOnlyFilter()
-    )
-    val schemaScan = new Scan()
 
-    schemaScan.addColumn(Bytes.toBytes("d"), Bytes.toBytes("schemaPostChange"))
-    schemaScan.setFilter(filters)
-    val rdd = hbc.hbaseRDD("schema_history:dw", schemaScan, { r: (ImmutableBytesWritable, Result) => r._2 })
+  /** Extract Spark schema from MySQL to HBase dump
+    * @param tableName Original MySQL table name
+    * @param schemaTableName HBase schema table
+    * @param timestamp Closest epoch to desired version of the schema
+    * @return Spark schema
+    */
+  def getSchema(tableName: String, schemaTableName: String, timestamp: Long): StructType = {
 
-    StructType(transformSchema(tableName,
-      Bytes.toString(result.getFamilyMap(Bytes.toBytes("d")).get(Bytes.toBytes("schemaPostChange")))
-    ))
-  }
-
-  def main(cmdArgs: Array[String]): Unit = {
-    val args = parseArguments(cmdArgs)
-    val config = new ConfigParser(args.configPath)
-
-    init(args, config)
-
-    val schema: StructType = getSchema("Reservation")
-
-    // Scans the given HBase table into an RDD.
-    val hbaseRDD = hbc.hbaseRDD(args.hbaseTableName, scan, { r: (ImmutableBytesWritable, Result) => r._2 })
-
-    // Mapping every row in HBase to a Row object in a Spark Dataframe
-    // Note: familyMap has a custom comparator. The entries are sorted from newest to oldest.
-    // map.firstEntry() is the newest entry (with largest timestamp). This is different than the default behaviour
-    // of firstEntry() and lastEntry().
-
-    val rowRDD = hbaseRDD.map(hbaseRow => {
-      val familyMap = hbaseRow.getMap
-      transformMapToRow(familyMap, schema)
+    /* Replicator schema dumps are keyed on timestamp, except for the
+     * initial snapshot, which is keyed on "initial-snapshot".
+     *  We therefore define an explicit ordering that takes it into account
+     */
+    val keyOrdering: Ordering[Result] = Ordering.by[Result, Long]({ x: Result =>
+      val key = Bytes.toStringBinary(x.getRow())
+      key match {
+        case "initial-snapshot" => 0
+        case _ => key.toLong
+      }
     })
 
-    val dataFrame = hc.createDataFrame(rowRDD, schema)
-    dataFrame.write.mode(SaveMode.Overwrite).saveAsTable(args.hiveTableName)
+    /* get correct schema row: top(1) returns greatest (latest) key in [0,
+     * timestamp/now()].
+     */
+    val scan = new Scan()
+    if (timestamp > -1) scan.setTimeRange(0, timestamp)
+    scan.addColumn(Bytes.toBytes("d"), Bytes.toBytes("schemaPostChange"))
+    scan.setFilter(new FilterList(FilterList.Operator.MUST_PASS_ALL, new FirstKeyOnlyFilter(), new KeyOnlyFilter()))
+
+    val rdd = _hbc.hbaseRDD(schemaTableName, scan, { r: (ImmutableBytesWritable, Result) => r._2 })
+    val row = rdd.top(1)(keyOrdering).last.getRow()
+
+    /* get correct schema json dump: since we want to use
+     * HBaseContext.hbaseRDD, we need to use a Scan object. However,
+     * we can't pass (row, row), because the latter is exclusive,
+     * which means we get the empty set. Therefore, we start the scan
+     * at the (inclusive) row, and grab the top(1).reverse, which
+     * returns the smallest (earliest) row. */
+    val fullScan = new Scan(row)
+    fullScan.addColumn(Bytes.toBytes("d"), Bytes.toBytes("schemaPostChange"))
+    fullScan.setFilter(new FirstKeyOnlyFilter())
+
+    val fullRDD = _hbc.hbaseRDD(schemaTableName, fullScan, { r: (ImmutableBytesWritable, Result) => r._2 })
+
+    val value =
+      Bytes.toStringBinary(
+        fullRDD.top(1)(keyOrdering).reverse.last.getMap()
+          .get(Bytes.toBytes("d"))
+          .get(Bytes.toBytes("schemaPostChange"))
+          .lastEntry()
+          .getValue()
+      )
+
+    transformSchema(tableName, value)
   }
+
 
   /**
     * Transforms the data in a hashmap into a Row object.
