ae6fcdf745138fa5cc5315549e136ce3ffd88b13
Pedro Silva <pedro.silva@booking.com>
2016-09-30 14:52:46 +0200

    remove unneccessary config options

--- hbase-snapshotter/src/main/scala/com/booking/spark/ConfigParser.scala
+++ hbase-snapshotter/src/main/scala/com/booking/spark/ConfigParser.scala
@@ -2,11 +2,13 @@ package com.booking.spark
 
 import java.nio.file.{Paths, Files}
 import java.util
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-
-// TODO: replace with scala.util.parsing.yaml
 import org.yaml.snakeyaml.Yaml
 import scala.collection.JavaConverters._
+
+case class IllegalFormatException(
+  message: String,
+  cause: Throwable
+) extends RuntimeException(message, cause)
 
 /**
  * Simple utility class for loading configuration from yml file
@@ -14,36 +16,28 @@ import scala.collection.JavaConverters._
  *
  * hbase:
  * zookeeper_quorum:  ['hbase-zk1-host', 'hbase-zkN-host']
- * schema: ['family1:qualifier1', 'familyN:qualifierN']
- * hive:
- * default_null: "DEFAULT NULL VALUE"
  */
 class ConfigParser(configPath: String) {
   val yaml = new Yaml()
   val in = Files.newInputStream(Paths.get(configPath))
   val config = yaml.load(in).asInstanceOf[util.Map[String, util.Map[String, Object]]]
+
+  try {
+    getZooKeeperQuorum
+  }
+  catch {
+    case e: Exception =>
+      throw IllegalFormatException(
+        "The yaml config file is not formatted correctly." +
+          "Check readme.md file for more information.",
+        e
+      )
+  }
+
   def getZooKeeperQuorum(): String = {
     return config.get("hbase")
       .get("zookeeper_quorum")
       .asInstanceOf[util.ArrayList[String]]
       .asScala.mkString(", ")
-  }
-
-  def getSchema: StructType = {
-    val columnList = config.get("hbase")
-      .get("schema")
-      .asInstanceOf[util.ArrayList[String]]
-      .asScala
-
-    return StructType(columnList.map(columnName => {
-      StructField(columnName, StringType, false)
-    }))
-  }
-
-  def getDefaultNull: String = {
-    config.get("hive")
-      .asScala
-      .getOrElse("default_null", "NULL")
-      .asInstanceOf[String]
   }
 }
